54d949e0c053f20ad6294d49fbaa31d04265ad5d
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-07 16:36:18 +0200

    add form ( without logic )

--- index.php
+++ index.php
@@ -3,21 +3,21 @@ $hotels = [
 
     [
         'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
+        'description' => 'Hotel Belvedere Description',
         'parking' => true,
         'vote' => 4,
         'distance_to_center' => 10.4
     ],
     [
         'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
+        'description' => 'Hotel Futuro Description',
         'parking' => true,
         'vote' => 2,
         'distance_to_center' => 2
     ],
     [
         'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
+        'description' => 'Hotel Rivamare Description',
         'parking' => false,
         'vote' => 1,
         'distance_to_center' => 1
@@ -31,7 +31,7 @@ $hotels = [
     ],
     [
         'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
+        'description' => 'Hotel Milano Description',
         'parking' => true,
         'vote' => 2,
         'distance_to_center' => 50
@@ -59,19 +59,41 @@ $hotels = [
 <body class="bg-info">
     <div class="container mt-5">
         <h1 class="pb-3 text-center">HOTEL LIST : </h1>
+        <form class="d-flex mb-3 align-items-center justify-content-center" style="gap:2rem">
+            <label for="select">Parking : </label>
+            <select class="form-select w-25 align-items-center" aria-label="Default select example" id="select">
+                <option selected>Choose</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+            </select>
+
+            <label for="vote">Vote</label>
+            <select class="form-select w-25 align-items-center" aria-label="Default select example" id="select">
+                <option selected>Select </option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+
+            <button type="button" class="btn btn-primary">Search</button>
+
+            <button type="reset" class="btn btn-secondary">Reset</button>
+        </form>
         <table class="table table-hover table-dark">
             <thead>
                 <tr>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Descrizione</th>
-                    <th scope="col">Parcheggio</th>
-                    <th scope="col">Voto</th>
-                    <th scope="col">Distanza dal centro</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking space</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance from the center</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($hotels as $hotel) { ?>
-                <tr>
+                <tr style="cursor:pointer">
                     <td><?= $hotel['name'] ?></td>
                     <td><?= $hotel['description'] ?></td>
                     <td><?= $hotel['parking'] ?></td>
